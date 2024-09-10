84fd0fffffbb37506d1f6d36062db405c050636e
Daniel Ozotha <oreziter@gmail.com>
2024-09-10 18:27:52 -0500

    test

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,38 @@
 // STRETCH
+const cars = [
+  {
+    vin: '5XXG34J54RG219816',
+    make: 'kia',
+    model: 'k5',
+    mileage:  186011 ,
+    title: 'clean',
+    transmission: 'automatic',
+  },
+  {
+  vin: '5XXG34J54RG219816',
+    make: 'honda',
+    model: 'cvic',
+    mileage:  136011 ,
+    title: 'salvage',
+  },
+
+  {
+    vin: '5XXG34J54RG219816',
+    make: 'mazda',
+    model: 'gt',
+    mileage:  176011, 
+  },
+]
+
+// exports.seed = function(knex) {
+//   return knex('cars')
+//   .truncate().then(() => {
+//    return knex('cars').insert(cars)
+//   })  
+// }
+
+exports.seed = async function (knex) {
+  await knex('cars').truncate()
+  await knex('cars').insert(cars)
+}
+
